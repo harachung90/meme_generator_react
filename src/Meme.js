901eeb561a8b9df73a93075e48e6915feacbbfce
Hara Chung <harachung90@gmail.com>
2022-12-11 19:40:48 +1100

    Third commit. Added memesData.js and a handleClick function for selecting a random meme image

--- src/Meme.js
+++ src/Meme.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import meme from './img.png';
+import memesData from "./memesData";
 
 export default function Meme() {
+
+    function randomMeme() {
+        const memesArray = memesData.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArray.length);
+        //console.log(memesArray[randomNum]);
+
+        const url = memesArray[randomNum].url;
+        console.log(url);
+    }
+
     return(
         <div className="meme">
             <form className="inputs">
                 <input type="text" className="top_line" placeholder="Shut up"/>
                 <input type="text" className="bottom_line" placeholder="and take my money" />
-                <button>Get a new meme image 🖼</button>
+                <button onClick={randomMeme}>Get a new meme image 🖼</button>
             </form>
-
-
         </div>
 
     )
